6d343076af73de16d5a2d412e4a173e96aff4877
vasker <yarinvasket@gmail.com>
2020-03-01 19:44:45 +0200

    Initial commit

---
+++ PrintTable.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class PrintTable : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+            string db = "Database.mdb";
+            MyAdoHelperAccess.ConnectToDb(db);
+            string sql = "select * from tbl_users";
+            string str = MyAdoHelperAccess.printDataTable(db, sql);
+            Response.Write(str);
+        }
+    }
+}
