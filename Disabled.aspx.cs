51b18a4bd540a055acda4920f8c17d194f46316b
vasker <yarinvasket@gmail.com>
2020-06-12 09:35:08 +0300

    fix

--- Disabled.aspx.cs
+++ Disabled.aspx.cs
@@ -9,16 +9,22 @@ public partial class Disabled : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string uname = (string)Session["username"];
-        string password = (string)Session["password"];
-        string db = "Database.mdb";
-
-        string sql = "select * from tbl_users where uname='" + uname + "' and upass='" + password + "';";
-        string selected = Request["accessibility"];
-
-        if (MyAdoHelperAccess.IsExist(db, sql))
+        if (Page.IsPostBack)
         {
-            string query = "update tbl_users\nset accessibility = " + selected + "\nwhere uname = " + uname;
+            string uname = (string)Session["username"];
+            string password = (string)Session["password"];
+            string db = "Database.mdb";
+
+            string sql = "select * from tbl_users where uname='" + uname + "' and upass='" + password + "';";
+            bool selected = Request["accessibility"] == "on";
+
+            if (MyAdoHelperAccess.IsExist(db, sql))
+            {
+                string query = "update tbl_users\nset accessibility = " + selected + "\nwhere uname = '" + uname + "';";
+                MyAdoHelperAccess.DoQuery(db, query);
+            }
+
+            Response.Redirect("index.aspx");
         }
     }
 }
